ba5dbcfa738c220076a618e10692b40ecb675474
Matthew Lai <huatao.lai@gmail.com>
2025-07-19 21:36:28 +1000

    feat: add firebase (#2)
    * test: add dependencies and implement initial test
    
    * feat: add firebase
    
    * fix: remove top level await
    
    * feat: add meta to child route
    
    * build: set ssr to false
    
    * fix: resolve ssr = false issues
    
    * build: add vercel.json

---
+++ app/routes/child-route.tsx
@@ -0,0 +1,51 @@
+import { collection, CollectionReference, getDocs } from "firebase/firestore";
+import { Child } from "~/child/child";
+import app, { db } from "~/firebase";
+import type { Route } from "./+types/child-route";
+import type { User } from "~/model/user";
+import type { Team } from "~/model/team";
+import type { Line } from "~/model/line";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App Child Route" },
+    { name: "description", content: "React Router App Child Route" },
+  ];
+}
+
+export async function clientLoader() {
+  const [usersSnap, teamsSnap, linesSnap] = await Promise.all([
+    getDocs(collection(db, "users") as CollectionReference<User>),
+    getDocs(collection(db, " teams") as CollectionReference<Team>),
+    getDocs(collection(db, "lines") as CollectionReference<Line>),
+  ]);
+
+  const users = usersSnap.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  console.log("Users:", users);
+
+  const teams = teamsSnap.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  console.log("Teams:", teams);
+
+  const lines = linesSnap.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+
+  console.log("Lines:", lines);
+
+  return { users, teams, lines };
+}
+
+export default function ChildRoute({ loaderData }: Route.ComponentProps) {
+  const { users, teams, lines } = loaderData;
+
+  return <Child firebaseOptions={app.options} users={users} />;
+}
