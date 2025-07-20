a083d7e67055d1db42b5f99a2f7980ba4e271e0c
Matthew <huatao.lai@gmail.com>
2025-07-20 16:18:05 +1000

    feat: adjust OptionContainer border colour

--- app/option-container/option-container.tsx
+++ app/option-container/option-container.tsx
@@ -1,9 +1,10 @@
 export default function OptionContainer() {
   return (
     <div
-      className="flex w-full p-4 rounded-lg border-1 
+        className="flex w-full rounded-lg gap-2 p-2 border-1 
     bg-gray-400 dark:bg-gray-800 
-    border-purple-500 dark:border-purple-600"
-    ></div>
+            border-purple-500 dark:border-purple-700"
+      >
+      </div>
   );
 }
