506aab29ad52bc6db2bdc66b5c495deff6e5b791
Matthew <huatao.lai@gmail.com>
2025-07-20 16:24:15 +1000

    feat: add Options array as OptionContainer prop

--- app/option-container/option-container.tsx
+++ app/option-container/option-container.tsx
@@ -1,8 +1,16 @@
+import type { BaseFirebaseDocument } from "~/model/base-firebase-document";
+import Option from "./option";
 type OptionContainerProps = {
   optionContainerName: string;
+  options: BaseFirebaseDocument[];
+};
 export default function OptionContainer({
   optionContainerName,
+  options,
 }: OptionContainerProps) {
+  const handleClick = (id: string) => {
+  };
+
   return (
     <div>
       <p className="px-2 font-bold text-xl">{optionContainerName}</p>
@@ -11,6 +19,17 @@ export default function OptionContainer({
     bg-gray-400 dark:bg-gray-800 
             border-purple-500 dark:border-purple-700"
       >
+        {options.map((option) => (
+          <Option
+            key={option.id}
+            id={option.id}
+            name={option.name}
+            selected={selectedIds.includes(option.id)}
+            onClick={(id) => {
+              handleClick(id);
+            }}
+          />
+        ))}
       </div>
       </div>
   );
