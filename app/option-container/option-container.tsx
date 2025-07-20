d317e25715a5194b4d93517c9f7a0e172e754d1a
Matthew <huatao.lai@gmail.com>
2025-07-20 16:19:41 +1000

    feat: add optionContainer name parameter

--- app/option-container/option-container.tsx
+++ app/option-container/option-container.tsx
@@ -1,10 +1,17 @@
-export default function OptionContainer() {
+type OptionContainerProps = {
+  optionContainerName: string;
+export default function OptionContainer({
+  optionContainerName,
+}: OptionContainerProps) {
   return (
+    <div>
+      <p className="px-2 font-bold text-xl">{optionContainerName}</p>
     <div
         className="flex w-full rounded-lg gap-2 p-2 border-1 
     bg-gray-400 dark:bg-gray-800 
             border-purple-500 dark:border-purple-700"
       >
+      </div>
       </div>
   );
 }
