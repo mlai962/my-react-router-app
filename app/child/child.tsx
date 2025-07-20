21add2317aa1fb5f8f07053e14c3bd85333d0cf9
Matthew <huatao.lai@gmail.com>
2025-07-20 16:27:01 +1000

    feat: add OptionContainers to child route

--- app/child/child.tsx
+++ app/child/child.tsx
@@ -1,14 +1,21 @@
 import { type FirebaseOptions } from "firebase/app";
+import { useState } from "react";
+import type { Line } from "~/model/line";
+import type { Team } from "~/model/team";
 import type { User } from "~/model/user";
 import OptionContainer from "~/option-container/option-container";
 
-export function Child({
-  firebaseOptions,
-  users,
-}: {
+type ChildProps = {
   firebaseOptions: FirebaseOptions;
   users: User[];
-}) {
+  teams: Team[];
+  lines: Line[];
+};
+
+export function Child({ firebaseOptions, users, teams, lines }: ChildProps) {
+  const [selectedUserIds, setSelectedUserIds] = useState<string[]>([]);
+  const userMap = new Map(users.map((user) => [user.id, user]));
+
   return (
     <main className="flex-col p-8 space-y-4">
       <div className="flex-col p-4 bg-blue-950 border-2 border-blue-300 rounded-xl w-full h-full">
@@ -27,7 +34,28 @@ export function Child({
         ))}
       </div>
 
-      <OptionContainer></OptionContainer>
+      <OptionContainer
+        optionContainerName="Users"
+        options={users}
+        maxOptionsSelectable={2}
+        onSelectionChange={(selectionOrder) => {
+          setSelectedUserIds(selectionOrder);
+        }}
+      ></OptionContainer>
+
+      <OptionContainer
+        optionContainerName="Teams"
+        options={teams}
+        maxOptionsSelectable={2}
+        onSelectionChange={(selectionOrder) => {}}
+      ></OptionContainer>
+
+      <OptionContainer
+        optionContainerName="Lines"
+        options={lines}
+        maxOptionsSelectable={1}
+        onSelectionChange={(selectionOrder) => {}}
+      ></OptionContainer>
     </main>
   );
 }
