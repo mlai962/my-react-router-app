974a05990f29ca2aea2d4cd2d40612634e53ad13
Matthew <huatao.lai@gmail.com>
2025-07-26 22:56:59 +1000

    feat: implement getting bets from firebase

--- app/routes/bet-log-route.tsx
+++ app/routes/bet-log-route.tsx
@@ -1,20 +1,27 @@
-import { collection, CollectionReference, getDocs } from "firebase/firestore";
+import {
+  collection,
+  CollectionReference,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 import { BetLog } from "~/bet-log/bet-log";
 import { db } from "~/firebase";
 import type { Route } from "./+types/bet-log-route";
 import type { User } from "~/model/user";
 import type { Team } from "~/model/team";
 import type { Line } from "~/model/line";
+import { Bet, type BetDto } from "~/model/bet";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Bet Log" }, { name: "description", content: "Bet Log" }];
 }
 
 export async function clientLoader() {
-  const [usersSnap, teamsSnap, linesSnap] = await Promise.all([
+  const [usersSnap, teamsSnap, linesSnap, betsSnap] = await Promise.all([
     getDocs(collection(db, "users") as CollectionReference<User>),
     getDocs(collection(db, " teams") as CollectionReference<Team>),
     getDocs(collection(db, "lines") as CollectionReference<Line>),
+    getDocs(collection(db, "bets") as CollectionReference<BetDto>),
   ]);
 
   const users = usersSnap.docs.map((doc) => ({
@@ -31,6 +38,22 @@ export async function clientLoader() {
     ...doc.data(),
     id: doc.id,
   }));
+
+  const bets = await Promise.all(
+    betsSnap.docs.map(async (doc) => {
+      const docData = doc.data();
+
+      const userA = await getDoc(docData.userA);
+      const userB = await getDoc(docData.userB);
+      const teamA = await getDoc(docData.teamA);
+      const teamB = await getDoc(docData.teamB);
+      const line = await getDoc(docData.line);
+
+      return new Bet(docData, doc.id, userA, userB, teamA, teamB, line);
+    })
+  );
+
+  console.log(bets);
 
   return { users, teams, lines };
 }
