0e9970ad329c21909a4890b330cd49d861c60e7d
Matthew <huatao.lai@gmail.com>
2025-07-27 15:12:49 +1000

    refactor: add placeholder text and svg path inputs to BetAmountInput

--- app/inputs/bet-amount-input.tsx
+++ app/inputs/bet-amount-input.tsx
@@ -1,8 +1,14 @@
 type BetAmountInputProps = {
   onChange: (amount: number) => void;
+  placeholder: string;
+  svgPath: string;
 };
 
-export default function BetAmountInput({ onChange }: BetAmountInputProps) {
+export default function BetAmountInput({
+  onChange,
+  placeholder,
+  svgPath,
+}: BetAmountInputProps) {
   return (
     <div
       className="flex w-max rounded-lg gap-2 p-2 border-1 items-center
@@ -25,14 +31,14 @@ export default function BetAmountInput({ onChange }: BetAmountInputProps) {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
-          d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
+          d={svgPath}
         />
       </svg>
 
       <input
         className="w-52 h-16 focus:outline-none text-xl font-semibold"
         type="text"
-        placeholder="bet amount..."
+        placeholder={placeholder}
         onChange={(e) => {
           const raw = e.target.value;
 
