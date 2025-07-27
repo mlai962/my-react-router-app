d2cb376c4531a0a948011ae53ec7651faa7ac2e5
Matthew <huatao.lai@gmail.com>
2025-07-27 15:39:00 +1000

    feat: add odds number input

--- app/bet-log/bet-log.tsx
+++ app/bet-log/bet-log.tsx
@@ -49,6 +49,7 @@ export function BetLog({ users, teams, lines, bets }: BetLogProps) {
     value: 0.5,
   });
 
+  const [odds, setOdds] = useState<number>(0);
   const [betAmount, setBetAmount] = useState<number>(0);
 
   const handleBetSubmit = () => {};
@@ -147,6 +148,12 @@ export function BetLog({ users, teams, lines, bets }: BetLogProps) {
           }}
           type={BinaryOptionType.HANDICAP}
         ></BinaryOptionAndNumberInput>
+
+        <NumberInput
+          onChange={(odds) => setOdds(odds)}
+          placeholder="odds..."
+          svgPath="M8.891 15.107 15.11 8.89m-5.183-.52h.01m3.089 7.254h.01M14.08 3.902a2.849 2.849 0 0 0 2.176.902 2.845 2.845 0 0 1 2.94 2.94 2.849 2.849 0 0 0 .901 2.176 2.847 2.847 0 0 1 0 4.16 2.848 2.848 0 0 0-.901 2.175 2.843 2.843 0 0 1-2.94 2.94 2.848 2.848 0 0 0-2.176.902 2.847 2.847 0 0 1-4.16 0 2.85 2.85 0 0 0-2.176-.902 2.845 2.845 0 0 1-2.94-2.94 2.848 2.848 0 0 0-.901-2.176 2.848 2.848 0 0 1 0-4.16 2.849 2.849 0 0 0 .901-2.176 2.845 2.845 0 0 1 2.941-2.94 2.849 2.849 0 0 0 2.176-.901 2.847 2.847 0 0 1 4.159 0Z"
+        ></NumberInput>
 
         <NumberInput
           onChange={(amount) => setBetAmount(amount)}
