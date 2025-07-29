c54d13655780559ca704edcc20277504eb5182b3
Matthew <huatao.lai@gmail.com>
2025-07-29 21:45:02 +1000

    refactor: remove unneeded css link

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="./app.css" rel="stylesheet" />
         <Meta />
         <Links />
       </head>
