7299ebd165a47719869aab9e6903f65f76e22bdc
Matthew <huatao.lai@gmail.com>
2025-07-31 23:22:37 +1000

    refactor: change modal x button to use an svg x

--- app/modal/modal.tsx
+++ app/modal/modal.tsx
@@ -51,11 +51,27 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
         onClick={stopPropagation}
       >
         <button
-          className="absolute top-0 right-2 text-xl text-white cursor-pointer"
+          className="absolute top-1 right-1 text-xl text-white cursor-pointer"
           onClick={onClose}
           aria-label="Close modal"
         >
-          ×
+          <svg
+            className="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18 17.94 6M18 18 6.06 6"
+            />
+          </svg>
         </button>
         {children}
       </div>
