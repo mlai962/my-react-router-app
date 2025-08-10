f22b8e6b79ce2fe49d613ec091bda49f9a3de4e2
Matthew <huatao.lai@gmail.com>
2025-08-10 14:46:32 +1000

    style: redesign bet history

--- app/bet-log/bet-history.tsx
+++ app/bet-log/bet-history.tsx
@@ -22,146 +22,138 @@ export default function BetHistory({
   currentBetIdBeingSettled,
 }: BetHistoryProps) {
   return (
-    <div className="w-full h-max space-y-1">
-      {bets.map((bet) => {
-        const binaryLineOption: boolean =
-          bet.extras[EXTRA_BINARY_LINE_OPTION] ?? true;
-        const binaryLineValue: number =
-          bet.extras[EXTRA_BINARY_LINE_VALUE] ?? 0;
+    <div className="w-full h-max flex justify-center">
+      <div className="w-max space-y-1 flex flex-col">
+        {bets.map((bet) => {
+          const binaryLineOption: boolean =
+            bet.extras[EXTRA_BINARY_LINE_OPTION] ?? true;
+          const binaryLineValue: number =
+            bet.extras[EXTRA_BINARY_LINE_VALUE] ?? 0;
 
-        return (
-          <div
-            key={bet.id}
-            className="w-full h-max rounded-lg p-2 border-1 relative
-            text-purple-200
-            bg-gray-400 dark:bg-purple-950/10
-            border-purple-500 dark:border-purple-700"
-          >
+          return (
             <div
-              className={`w-full h-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${
-                isShowBetSettlementSpinner && currentBetIdBeingSettled == bet.id
-                  ? "opacity-20"
-                  : ""
-              }`}
+              key={bet.id}
+              className="rounded-lg py-2 px-4 border-1 relative
+              text-purple-200
+              bg-gray-400 dark:bg-purple-950/10
+              border-purple-500 dark:border-purple-700"
             >
-              <div>{bet.date.toDate().toDateString()}</div>
-
-              <div>
-                {bet.userA.name} vs {bet.userB.name}
-              </div>
-
-              <div>
-                {bet.teamA.name} vs {bet.teamB.name}
-              </div>
-
-              <div>
-                {bet.map} {bet.line.name}{" "}
-                {bet.line.lineType === LineType.NONE
-                  ? ""
-                  : bet.line.lineType === LineType.OVER_UNDER
-                  ? (binaryLineOption ? "o" : "u") + binaryLineValue
-                  : (binaryLineOption ? "+" : "-") + binaryLineValue}
-              </div>
-
-              <div className="flex w-full items-center">
-                <div className="w-1/3">@{bet.odds.toFixed(2)}x</div>
-                <div className="w-1/3">${bet.betAmount}</div>
-              </div>
-
-              <div className="flex w-full">
-                <div className="w-1/3 flex items-center">
-                  <label className="flex items-center text-sm font-medium hover:cursor-pointer">
-                    <input
-                      id={"radio-unsettled-" + bet.id}
-                      type="radio"
-                      value=""
-                      name={"settle-bet-radio-group" + bet.id}
-                      checked={bet.winner.length === 0}
-                      onChange={(e) =>
-                        handleBetSettlement(bet.id, e.target.value)
-                      }
-                      className="w-4 h-4 me-1 
-                    accent-purple-800 dark:accent-purple-600
-                    bg-gray-100 dark:bg-gray-700
-                    border-gray-300 dark:border-gray-600
-                    focus:ring-0 hover:cursor-pointer"
-                    />
-                    Unsettled
-                  </label>
+              <div
+                className={`w-full h-max flex flex-col items-center ${
+                  isShowBetSettlementSpinner &&
+                  currentBetIdBeingSettled == bet.id
+                    ? "opacity-20"
+                    : ""
+                }`}
+              >
+                <div className="flex gap-2">
+                  <div>
+                    {bet.date
+                      .toDate()
+                      .toLocaleDateString("en-US", { weekday: "long" })}
+                  </div>
+                  <div>{bet.date.toDate().toLocaleDateString()}</div>
                 </div>
 
-                <div className="w-1/3 flex items-center">
-                  <label className="flex items-center text-sm font-medium hover:cursor-pointer">
-                    <input
-                      id={"radio-userA-" + bet.id}
-                      type="radio"
-                      value="userA"
-                      name={"settle-bet-radio-group" + bet.id}
-                      checked={bet.winner === "userA"}
-                      onChange={(e) =>
-                        handleBetSettlement(bet.id, e.target.value)
-                      }
-                      className="w-4 h-4 me-1 
-                    accent-purple-800 dark:accent-purple-600
-                    bg-gray-100 dark:bg-gray-700
-                    border-gray-300 dark:border-gray-600
-                    focus:ring-0"
-                    />
+                <div className="w-full flex">
+                  <div className="w-[45%] flex justify-end items-center gap-2">
                     {bet.userA.name}
-                  </label>
-                </div>
-
-                <div className="w-1/3 flex items-center">
-                  <label className="flex items-center text-sm font-medium hover:cursor-pointer">
                     <input
-                      id={"radio-userB-" + bet.id}
-                      type="radio"
+                      id={"checkbox-userA-" + bet.id}
+                      type="checkbox"
+                      value="userA"
+                      name={"checkbox-userA-" + bet.id}
+                      checked={bet.winner === "userA"}
+                      onChange={(e) => {
+                        handleBetSettlement(
+                          bet.id,
+                          e.target.checked ? "userA" : ""
+                        );
+                      }}
+                      className="w-4 h-4
+                        accent-purple-800 dark:accent-purple-600
+                        bg-gray-100 dark:bg-gray-700
+                        border-gray-300 dark:border-gray-600
+                        focus:ring-0 hover:cursor-pointer"
+                    />
+                  </div>
+                  <div className="w-[10%] text-center">v</div>
+                  <div className="w-[45%] flex justify-start items-center gap-2">
+                    <input
+                      id={"checkbox-userB-" + bet.id}
+                      type="checkbox"
                       value="userB"
-                      name={"settle-bet-radio-group" + bet.id}
+                      name={"checkbox-userB-" + bet.id}
                       checked={bet.winner === "userB"}
-                      onChange={(e) =>
-                        handleBetSettlement(bet.id, e.target.value)
-                      }
-                      className="w-4 h-4 me-1 
-                    accent-purple-800 dark:accent-purple-600
-                    bg-gray-100 dark:bg-gray-700
-                    border-gray-300 dark:border-gray-600
-                    focus:ring-0"
+                      onChange={(e) => {
+                        handleBetSettlement(
+                          bet.id,
+                          e.target.checked ? "userB" : ""
+                        );
+                      }}
+                      className="w-4 h-4 ms-1
+                        accent-purple-800 dark:accent-purple-600
+                        bg-gray-100 dark:bg-gray-700
+                        border-gray-300 dark:border-gray-600
+                        focus:ring-0 hover:cursor-pointer"
                     />
                     {bet.userB.name}
-                  </label>
+                  </div>
+                </div>
+
+                <div className="w-full flex">
+                  <div className="w-[45%] text-right">{bet.teamA.name}</div>
+                  <div className="w-[10%] text-center">v</div>
+                  <div className="w-[45%] text-left">{bet.teamB.name}</div>
+                </div>
+
+                <div className="flex gap-2">
+                  <div>
+                    {bet.map}{" "}
+                    {bet.line.lineType === LineType.HANDICAP
+                      ? (binaryLineOption ? "+" : "-") + binaryLineValue + " "
+                      : ""}
+                    {bet.line.name}{" "}
+                    {bet.line.lineType === LineType.OVER_UNDER
+                      ? (binaryLineOption ? "o" : "u") + binaryLineValue
+                      : ""}
+                  </div>
+                  <div>
+                    @{bet.odds.toFixed(2)} ${bet.betAmount}
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white 
+              <svg
+                className="w-6 h-6 text-gray-800 dark:text-white 
                 hover:text-red-500/75 cursor-pointer absolute top-1 right-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-              onClick={() => handleBetDeletion(bet.id)}
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-              />
-            </svg>
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+                onClick={() => handleBetDeletion(bet.id)}
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                />
+              </svg>
 
-            <Spinner
-              isShowSpinner={
-                isShowBetSettlementSpinner && currentBetIdBeingSettled == bet.id
-              }
-            />
-          </div>
-        );
-      })}
+              <Spinner
+                isShowSpinner={
+                  isShowBetSettlementSpinner &&
+                  currentBetIdBeingSettled == bet.id
+                }
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
